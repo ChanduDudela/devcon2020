050a1009beb1a15243f140259a34517e088435ce
CD065208 <chandrakanth.dudela@cerner.com>
2020-06-08 14:55:06 -0400

    2 sum

--- problem/src/main/java/aws/onlineassesment/TwoSum.java
+++ problem/src/main/java/aws/onlineassesment/TwoSum.java
@@ -6,12 +6,18 @@ import java.util.Arrays;
 //https://leetcode.com/discuss/interview-question/356960
 public class TwoSum {
 
+    public static void main(String[] args) {
+        TwoSum twoSum = new TwoSum();
+
+        System.out.println(twoSum.twoSum_NoOfUniquePairs(new int[] {1, 1, 2, 45, 46, 46}, 47));
+    }
+
     //brute force - quadratic time O(n^2)
     public int[] twoSum(int[] nums, int target) {
-        for(int i=0; i< nums.length; i++){
-            for (int j = i+1; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target){
-                    return new int[]{i,j};
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
                 }
             }
         }
@@ -20,7 +26,7 @@ public class TwoSum {
 
     //with O(n log n) time
     public int[] twoSum_NLogN_time(int[] nums, int target) {
-        target= target-30;
+        target = target - 30;
 
         int[] result = new int[2];
         int[] copyOfNums = Arrays.copyOf(nums, nums.length);
@@ -28,9 +34,9 @@ public class TwoSum {
         //quick sort O(n log(n))
         Arrays.sort(copyOfNums);
 
-        int i =0;
+        int i = 0;
         int a = 0, b = 0;
-        int j = nums.length-1;
+        int j = nums.length - 1;
 
         while (i < j) {
             if (copyOfNums[i] + copyOfNums[j] > target) {
@@ -45,7 +51,7 @@ public class TwoSum {
         }
 
         for (int k = 0; k < nums.length; k++) {
-            if(nums[k] == a){
+            if (nums[k] == a) {
                 result[0] = k;
                 break;
             }
@@ -57,6 +63,38 @@ public class TwoSum {
             }
         }
 
-       return result;
+        return result;
+    }
+
+    //https://leetcode.com/discuss/interview-question/372434
+    //with O(n log n) time
+    public int twoSum_NoOfUniquePairs(int[] arr, int target) {
+
+        Arrays.sort(arr);
+
+        int i = 0;
+        int j = arr.length - 1;
+        int result = 0;
+
+        while (i < j) {
+            if (arr[i] + arr[j] > target) {
+                j--;
+            } else if (arr[i] + arr[j] < target) {
+                i++;
+            } else {
+                result++;
+                i++;
+                j--;
+
+                while (i < j && arr[i] == arr[i - 1]) {
+                    i++;
+                }
+                while (i < j && arr[j] == arr[j + 1]) {
+                    j--;
+                }
+            }
+        }
+
+        return result;
     }
 }
