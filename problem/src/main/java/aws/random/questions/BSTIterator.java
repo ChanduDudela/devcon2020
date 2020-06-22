add7a16d1018538f819d7204ae6b139bfa4e4b43
CD065208 <chandrakanth.dudela@cerner.com>
2020-06-22 00:18:29 -0400

    more problems

---
+++ problem/src/main/java/aws/random/questions/BSTIterator.java
@@ -0,0 +1,63 @@
+package aws.random.questions;
+
+import java.util.Stack;
+
+//https://leetcode.com/problems/binary-search-tree-iterator
+// there is another approach of flattening the tree into an array.
+// See first approach in the solutions tab.
+public class BSTIterator {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    Stack<TreeNode> stack;
+
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+
+        traverseAllLeftNodes(root);
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode node = stack.pop();
+
+        //when this node has a right child,
+        //then we add all left nodes of this node to the stack
+        if (node.right != null) {
+            traverseAllLeftNodes(node.right);
+        }
+
+        return node.val;
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return stack.size() > 0;
+    }
+
+    // helper to traverse all left nodes (minimum values in the tree in-order traversal)
+    // and add it to the stack
+    private void traverseAllLeftNodes(TreeNode root) {
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+    }
+}
