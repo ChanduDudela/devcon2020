aaaef6411d7861512b2ecdd757a8ff9bc22cacdf
CD065208 <chandrakanth.dudela@cerner.com>
2020-06-26 11:54:37 -0400

    more problems

--- problem/src/main/java/aws/random/questions/LFU_Cache.java
+++ problem/src/main/java/aws/random/questions/LFU_Cache.java
@@ -1,71 +1,92 @@
 package aws.random.questions;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.Map;
 
 public class LFU_Cache {
 
     static class Entry {
-        int key;
+        final int key;
         int val;
-        int count;
-        Entry(int k, int v, int c) {
-            key = k;
-            val = v;
-            count = c;
+        int frequency;
+
+        public Entry(int key, int val) {
+            this.key = key;
+            this.val = val;
+            this.frequency = 1;
         }
     }
 
-    private HashMap<Integer, Entry> cache;
-    private HashMap<Integer, LinkedHashSet<Entry>> countMap;
-    private int capacity;
-    private int min = -1;
+    int capacity;
+    Map<Integer, Entry> keyToEntryMap;
+    Map<Integer, LinkedHashSet<Entry>> freqToEntryMap;
+    int minSize;
 
     public LFU_Cache(int capacity) {
         this.capacity = capacity;
-        cache = new HashMap<>();
-        countMap = new HashMap<>();
-        countMap.put(1, new LinkedHashSet<>());
+        keyToEntryMap = new HashMap<>();
+        freqToEntryMap = new HashMap<>();
+
+        minSize = 0;
     }
 
     public int get(int key) {
-        if (!cache.containsKey(key)) return -1;
-        Entry e = cache.get(key);
-        countMap.get(e.count).remove(e);
-        if (e.count == min && countMap.get(e.count).isEmpty()) {
-            countMap.remove(e.count);
-            min++;
+        if (!keyToEntryMap.containsKey(key)) {
+            return -1;
         }
-        e.count++;
-        if (!countMap.containsKey(e.count)) {
-            countMap.put(e.count, new LinkedHashSet<>());
+
+        Entry entry = keyToEntryMap.get(key);
+
+        freqToEntryMap.get(entry.frequency).remove(entry);
+
+        if (entry.frequency == minSize && freqToEntryMap.get(entry.frequency).isEmpty()) {
+            freqToEntryMap.remove(entry.frequency);
+            minSize++;
         }
-        countMap.get(e.count).add(e);
-        return e.val;
+
+        entry.frequency++;
+
+        if (!freqToEntryMap.containsKey(entry.frequency)) {
+            freqToEntryMap.put(entry.frequency, new LinkedHashSet<>());
+        }
+        freqToEntryMap.get(entry.frequency).add(entry);
+
+        return entry.val;
     }
 
     public void put(int key, int value) {
-        if (capacity <= 0) return;
-        if (cache.containsKey(key)) {
-            Entry e = cache.get(key);
-            e.val = value;
-            get(key);
+        if (capacity <= 0) {
             return;
         }
-        if (cache.size() == capacity) {
-            Iterator<Entry> it = countMap.get(min).iterator();
-            Entry e = it.next();
-            it.remove();
-            cache.remove(e.key);
-            if (!it.hasNext()) countMap.remove(min);
+
+        if (keyToEntryMap.containsKey(key)) {
+            Entry entry = keyToEntryMap.get(key);
+            entry.val = value;
+            get(key);
+
+            return;
         }
-        Entry ne = new Entry(key, value, 1);
-        cache.put(key, ne);
-        min = 1;
-        if (!countMap.containsKey(1)) {
-            countMap.put(1, new LinkedHashSet<>());
+
+        if (keyToEntryMap.size() == capacity) {
+
+            Entry entryWithMinFreq = freqToEntryMap.get(minSize).iterator().next();
+            freqToEntryMap.get(minSize).remove(entryWithMinFreq);
+
+            keyToEntryMap.remove(entryWithMinFreq.key);
+
+            if (freqToEntryMap.get(minSize).size() == 0) {
+                freqToEntryMap.remove(minSize);
+            }
         }
-        countMap.get(1).add(ne);
+
+        Entry newEntry = new Entry(key, value);
+        minSize = 1;
+
+        keyToEntryMap.put(key, newEntry);
+        if (!freqToEntryMap.containsKey(minSize)) {
+            freqToEntryMap.put(minSize, new LinkedHashSet<>());
+        }
+        freqToEntryMap.get(minSize).add(newEntry);
     }
 }
