858ea52f83c21a4f91af6d347763fec9ee28a33a
CD065208 <chandrakanth.dudela@cerner.com>
2021-06-23 23:40:08 -0400

    added more problems

---
+++ problem/src/main/java/ctci/arraysandstring/Problem4_Palindrome.java
@@ -0,0 +1,30 @@
+package ctci.arraysandstring;
+
+public class Problem4_Palindrome {
+    private static boolean isPalindrome(final String str) {
+        // should have even number of same characters, can have 1 odd character
+        int[] arr = new int[128];
+
+        for(int i=0; i < str.length(); i++) {
+            if(str.charAt(i) != ' ') {
+                arr[str.charAt(i)]++;
+            }
+        }
+
+        boolean check = false;
+        for(int i=0; i < str.length(); i++) {
+            if(arr[str.charAt(i)] % 2 == 1) {
+                if (check) {
+                    return false;
+                } else {
+                    check = true;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Problem4_Palindrome.isPalindrome("tacttttcoa"));
+    }
+}
---
+++ problem/src/main/java/ctci/arraysandstring/Problem5_OneEditAway.java
@@ -0,0 +1,51 @@
+package ctci.arraysandstring;
+
+public class Problem5_OneEditAway {
+    private static boolean oneEditAway(final String first, final String second) {
+        if (first.length() == second.length()) {
+            return oneEditReplace(first, second);
+        } else if (first.length() + 1 == second.length()) {
+            return oneEditInsert(first, second);
+        } else if (first.length() - 1 == second.length()) {
+            return oneEditInsert(second, first);
+        }
+        return false;
+    }
+
+    private static boolean oneEditReplace(String first, String second) {
+        boolean check = false;
+        for (int i = 0; i < first.length(); i++) {
+            if (first.charAt(i) != second.charAt(i)) {
+                if (check) {
+                    return false;
+                }
+                check = true;
+            }
+        }
+        return true;
+    }
+
+    //first is smaller, second is bigger string
+    private static boolean oneEditInsert(String first, String second) {
+        int index1 = 0;
+        int index2 = 0;
+
+        while (index1 < first.length() && index2 < second.length()) {
+            if (first.charAt(index1) != second.charAt(index2)) {
+                if (index1 != index2) {
+                    return false;
+                }
+
+                index2++;
+            } else {
+                index1++;
+                index2++;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Problem5_OneEditAway.oneEditAway("pale", "ple"));
+    }
+}
